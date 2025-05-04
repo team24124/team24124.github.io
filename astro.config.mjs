13722dea25996bb5b4e9194e557e5095812150e0
Jeff Khuu <heyjeffkhuu@gmail.com>
2025-05-04 10:43:54 -0600

    Reconfigured Google Analytics 4 implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown'
 import react from '@astrojs/react';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    react(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-  }),
+    react(), 
+    partytown({ config: { forward: ['dataLayer.push'] } })
   ],
   site: 'https://team24124.github.io',
 });
